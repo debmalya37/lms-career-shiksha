7b3c4f24b6b9e2dd59d0f7179a81c58f62deba61
Debmalya Sen <debmalyasen37@gmail.com>
2025-06-17 13:07:52 +0530

    #added admissiondata in admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -90,6 +90,9 @@ const AdminPanel = () => {
         <Link href="/admin/bannerAds" className="bg-blue-950 text-white p-6 rounded-lg shadow-md text-center">
           <h2 className="text-xl font-semibold">Manage Ads Banner</h2>
         </Link>
+        <Link href="/admin/admissionData" className="bg-blue-950 text-white p-6 rounded-lg shadow-md text-center">
+          <h2 className="text-xl font-semibold">Manage Admission data</h2>
+        </Link>
         <Link href="/admin/promocodes" className="bg-blue-950 text-white p-6 rounded-lg shadow-md text-center">
           <h2 className="text-xl font-semibold">Manage Promocodes</h2>
         </Link>
